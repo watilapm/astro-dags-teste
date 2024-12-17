f601376b438fe54017ab5b26830248a71f15799c
alexlopespereira <alexlopespereira@gmail.com>
2024-12-17 19:39:36 -0300

    third commit

--- dags/CoinCap.py
+++ dags/CoinCap.py
@@ -22,7 +22,7 @@ default_args={
     default_args=default_args,
     schedule="0 0 * * *",
     start_date=pendulum.from_format("2024-12-17", "YYYY-MM-DD").in_tz("UTC"),
-    catchup=False,
+    catchup=True,
     owner_links={
         "Alex Lopes": "mailto:alexlopespereira@gmail.com",
         "Open in Cloud IDE": "https://cloud.astronomer.io/cm3webulw15k701npm2uhu77t/cloud-ide/cm4t18rj8005301ktwkw8dox0/cm4t1ezx400n401lcmpgcwxae",
